39383a638c8a5221389d28fe576c4b5eef191b4d
ALUNO - SL103 <sl103@escolasebraesp.local>
2026-05-29 11:45:12 -0300

    first commit

--- api-login/src/main/kotlin/br/sp/etec/sebrae/api_login/repositore/BoletoRepository.kt
+++ api-login/src/main/kotlin/br/sp/etec/sebrae/api_login/repositore/BoletoRepository.kt
@@ -1,4 +1,9 @@
 package br.sp.etec.sebrae.api_login.repositore
 
-interface BoletoRepository {
+import br.sp.etec.sebrae.api_login.entity.Boleto
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BoletoRepository : JpaRepository<Boleto, Long> {
 }
